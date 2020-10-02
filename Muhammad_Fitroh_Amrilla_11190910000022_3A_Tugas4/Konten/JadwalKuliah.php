c25fb410595d4f40a55957385fed4381eab9411f
Muhammad Fitroh Amrilla <amrillafitroh@gmail.com>
2020-10-02 15:14:32 +0700

    tugas 5

---
+++ Muhammad_Fitroh_Amrilla_11190910000022_3A_Tugas4/Konten/JadwalKuliah.php
@@ -0,0 +1,63 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>mywebsite</title>
+
+    <link href="../style/style.css" type="text/css" rel="stylesheet"/>
+</head>
+
+
+<body>
+    <div id="Container">
+        <div id="header">
+            <h1>PERTEMUAN I -- SISTEM PAKAR</h1>
+        </div>
+    
+        <div id="sidebar">
+                <h3>Navigasi</h3>
+                <ul id="navmenu">
+                        <li><a href="../Index.php" class="selected">Profil </a></li>
+                        <li><a href="galeri.php" >Galeri </a></li>
+                        <li><a href="JadwalKuliah.php" >Jadwal kuliah </a></li>
+                </ul>
+        </div>
+            <div id="page">
+            <table border="1" width = "377" >
+                <tr> 
+                    <th width = "22" bgcolor = "#3399FF">No</th>
+                    <th width = "113" bgcolor = "#3399FF">Mata Kuliah</th>
+                    <th width = "65" bgcolor = "#3399FF">Hari</th>
+                    <th width = "76" bgcolor = "#3399FF">Waktu</th>
+                </tr>
+                <tr >
+                    <td>1</td>
+                    <td>Web Programming</td>
+                    <td>Rabu</td>
+                    <td>18.30-20.00</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Bahasa Arab</td>
+                    <td>Senin</td>
+                    <td>20.00-21.30</td>
+                </tr>
+                <tr >
+                    <td>3</td>
+                    <td>Algortima</td>
+                    <td>Selasa</td>
+                    <td>10.00-12.30</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Penelusuran Informasi</td>
+                    <td>Selasa</td>
+                    <td>13.00-15.00</td>
+                </tr>
+            </table>
+            </div>
+            <div id="clear"></div>
+            <div id="footer">
+                <p>&copy; 2020</p>
+            </div>
+    </div>
+</body>
+</html>
